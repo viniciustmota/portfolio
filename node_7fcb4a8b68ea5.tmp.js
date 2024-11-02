66499b162397f23b85960174b06f521b855f1425
viniciustmota <motaviny140@gmail.com>
2024-11-02 16:03:54 -0300

    vercel

---
+++ node_7fcb4a8b68ea5.tmp.js
@@ -0,0 +1,82 @@
+[
+  {
+    "id": 1,
+    "titulo": "Exploração da Tripulação do Chapéu de Palha: Personagens de One Piece",
+    "descricao": "Este projeto é uma experiência interativa dedicada aos fãs de One Piece , onde você pode explorar a história e as habilidades de cinco personagens icônicos da Tripulação do Chapéu de Palha. Com um design cativante e detalhado, cada página oferece uma inserção visual e informativa em personagens como Luffy, Zoro, Nami, Sanji e Usopp. Descubra curiosidades, veja ilustrações personalizadas e aprofunde-se nas características únicas de cada membro da tripulação e visual!",
+    "imagem": "./img/OnePiece.png",
+    "github": "https://github.com/viniciustmota/one-piece",
+    "link": "https://one-piece-two.vercel.app/"
+  },
+  {
+    "id": 2,
+    "titulo": "Jogos Educativos de Alfabetização: Aprendendo Palavras e Sílabas para Crianças de 6 a 8 Anos",
+    "descricao": "Este site oferece três jogos educativos interativos – Sílabas, Quebra-Cabeça de Palavras e Acerte a Letra – desenvolvidos especialmente para apoiar o aprendizado da leitura e escrita em crianças de 6 a 8 anos. Cada jogo foi criado para estimular a aprendizagem formação de palavras e o reconhecimento de sílabas e letras, tornando o processo de alfabetização divertido e envolvente Com o jogo Sílabas , as crianças combinam sons e sílabas para formar palavras , elas montam palavras a partir de letras e sílabas, e no Acerte a Letra , aprendem a identificar letras e posições nas palavras. Perfeito para o desenvolvimento linguístico inicial, nosso site oferece uma experiência lúdica e educativa!",
+    "imagem": "./img/Kids.png",
+    "github": "https://github.com/viniciustmota/kids",
+    "link": "https://kids-ochre.vercel.app/"
+  },
+  {
+    "id": 3,
+    "titulo": "Interatividade por Voz com JavaScript: Curso Prático da Alura",
+    "descricao": "Este projeto, desenvolvido no curso de JavaScript da Alura, explora a integração de controle de voz em interfaces web, utilizando bibliotecas e APIs modernas como a Web Speech API. Aprenda como criar aplicações interativas que falam a comandos de voz, permitindo uma experiência de usuário mais acessível e inovadora. Este projeto inclui exemplos práticos e casos de uso, como a ativação de funções através de comandos simples e navegação mãos-livres Ideal para desenvolvedores e entusiastas de JavaScript que desejam dominar o futuro da interatividade com controle por. voz!",
+    "imagem": "./img/NumeroSecreto.png",
+    "github": "https://github.com/viniciustmota/numero-secreto",
+    "link": "https://numero-secreto-one-theta.vercel.app/"
+  },
+  {
+    "id": 4,
+    "titulo": "Curso Alura: JavaScript – Consumindo e Tratando Dados de APIs",
+    "descricao": "Este repositório apresenta o projeto desenvolvido durante o curso 'JavaScript: Consumindo e Tratando Dados de uma API' da Alura, focado no consumo de dados de APIs RESTful usando JavaScript. O projeto explora técnicas para fazer requisições HTTP, processar respostas JSON e manipular dados com recursos modernos, como promessas e async/await É uma aplicação prática das habilidades adquiridas para integrar e tratar dados de APIs, com exemplos completos de código para quem deseja aprender ou revisar conceitos fundamentais de integração de APIs com JavaScript.",
+    "imagem": "./img/AluraBooksCadastro.png",
+    "github": "https://github.com/viniciustmota/js-consumindo-dados-api",
+    "link": "https://js-consumindo-dados-api-rose.vercel.app/"
+  },
+  {
+    "id": 5,
+    "titulo": "Armazenamento de Dados no Navegador com JavaScript",
+    "descricao": "Este projeto foi desenvolvido durante o curso de JavaScript da Alura, com foco em técnicas de armazenamento de dados no navegador. A aplicação demonstra como utilizar localStoragee sessionStoragepara salvar informações de forma persistente e temporária diretamente no navegador, sem necessidade de servidor . Com exemplos práticos de como armazenar, recuperar e manipular dados, o projeto é ideal para quem busca compreender e aplicar o armazenamento local em JavaScript, criando experiências de usuário mais personalizadas e responsivas.",
+    "imagem": "./img/Mochila.png",
+    "github": "https://github.com/viniciustmota/mochila-de-viajem",
+    "link": "https://mochila-de-viajem-nine.vercel.app/"
+  },
+  {
+    "id": 6,
+    "titulo": "To-Do List em JavaScript: Gerenciamento de Tarefas com Armazenamento Local",
+    "descricao": "Este projeto é uma aplicação de lista de tarefas (to-do list) desenvolvida em JavaScript, onde os usuários podem criar, visualizar e concluir suas tarefas diárias. Com interface intuitiva e armazenamento de dados no localStorage, o site permite que as tarefas permanecerão salvas mesmo após fechar o navegador. Ideal para quem deseja praticar manipulação de DOM, armazenamento local e interação dinâmica com JavaScript, este projeto demonstra como criar uma experiência de usuário prática e funcional para o gerenciamento de tarefas.",
+    "imagem": "./img/ToDoList.png",
+    "github": "https://github.com/viniciustmota/to-do-list",
+    "link": "https://to-do-list-mauve-xi.vercel.app/"
+  },
+  {
+    "id": 7,
+    "titulo": "Galeria Interativa de Personagens X-Men com JavaScript e CSS – Hover para Descrição",
+    "descricao": "Este projeto exibe uma galeria interativa com oito personagens icônicos dos X-Men, onde ao passar o mouse sobre cada personagem, uma descrição detalhada é mostrada. Desenvolvido com JavaScript, HTML e CSS, o site utiliza efeitos de hover para Revele informações e curiosidades sobre cada personagem, proporcionando uma experiência envolvente para os fãs. Ideal para praticar manipulação de eventos de mouse e animações CSS, este projeto tem estética e interatividade, oferecendo um exemplo de aplicação web com design intuitivo e responsivo.",
+    "imagem": "./img/XMen.png",
+    "github": "https://github.com/viniciustmota/x-men",
+    "link": "https://x-men.vercel.app/"
+  },
+  {
+    "id": 8,
+    "titulo": "Alura Books – Design Mobile-First com HTML, CSS e JavaScript",
+    "descricao": "Este projeto, 'Alura Books', é uma aplicação desenvolvida com o conceito mobile-first para melhorar a experiência de leitura em dispositivos móveis. Uma interface foi projetada com HTML, CSS e JavaScript, priorizando a responsividade e usabilidade em telas menores antes de se adaptarem a dispositivos maiores. O projeto explora práticas avançadas de layout responsivo, como flexbox , media queries e otimização de recursos, garantindo que o conteúdo e a navegação sejam intuitivos e acessíveis em smartphones. mobile-first e design responsivo.",
+    "imagem": "./img/AluraBooksMobile.png",
+    "github": "https://github.com/viniciustmota/alura-books",
+    "link": "https://alura-books-lake-three.vercel.app/"
+  },
+  {
+    "id": 9,
+    "titulo": "Teclado Interativo em JavaScript – Aprendendo com Alura Midi",
+    "descricao": "Este projeto é um teclado interativo desenvolvido durante o curso básico de JavaScript da Alura, conhecido como Alura Midi. O teclado permite que os usuários toquem diferentes sons ao pressionar as teclas, utilizando eventos de teclado e manipulação do DOM. Cada um A tecla foi associada a um som específico, proporcionando uma experiência divertida e educativa para quem está aprendendo os fundamentos do JavaScript. Ideal para iniciantes que desejam praticar eventos, áudio e interação em aplicações web, este projeto combina criatividade e aprendizado em programação.",
+    "imagem": "./img/AluraMidi.png",
+    "github": "https://github.com/viniciustmota/alura-midi",
+    "link": "https://alura-midi-azure.vercel.app/"
+  },
+  {
+    "id": 10,
+    "titulo": "Alura Foco – Aplicação de Estudo com Timer e Música",
+    "descricao": "Este projeto, chamado 'Alura Foco', foi desenvolvido durante o curso da Alura com foco na manipulação do DOM. A aplicação possui um timer que ajuda os usuários a gerenciar seu tempo de estudo, além de permitir a seleção de músicas para criar um ambiente de concentração ideal. Com uma interface intuitiva e funcional, o projeto demonstra como usar JavaScript para interagir com elementos HTML, gerenciar eventos e manipular áudio. otimizar o tempo de estudo.",
+    "imagem": "./img/AluraFokus.png",
+    "github": "https://github.com/viniciustmota/alura-fokus",
+    "link": "https://alura-fokus-eight.vercel.app/"
+  }
+]
